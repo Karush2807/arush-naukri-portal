f9c2fe21afd2244f2cfcff5e3079be0dd0c45e41
Karush2807 <134139172+Karush2807@users.noreply.github.com>
2024-02-16 20:36:34 +0000

    added some dynamic data to it

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask,render_template, jsonify
 
 app=Flask(__name__) #its just app name
 
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru, India',
+    'salary':'Rs. 10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi, India',
+    'salary':'Rs. 15,00,000'
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remote',
+    'salary':'Rs. 12,00,000'
+  },
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'location':'San Francisco, USA',
+    'salary':'$120,000'
+  }
+]
+
 @app.route('/') #yhn hm decide krte hai, ki kiss url pr kya hoga
 def helloworld():
-  return "namste duniya"
+  return render_template('home.html', jobs=JOBS, company_name='Arush')
+
+@app.route('/naukri')
+def naukri():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
